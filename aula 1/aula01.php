d8d017f89ade66524e64862b76a18125da577350
Wallyson Martins de Lima <wallysonml13@gmail.com>
2025-01-27 21:50:19 -0300

    27012025

---
+++ aula 1/aula01.php
@@ -0,0 +1,14 @@
+<?php
+$nome = "Leo";
+if($nome == "Leo"){
+    echo "hello world";
+}
+
+    // $variavel = "qualquer coisa";
+    // var_dump($variavel);
+    // echo "hello world\n";
+    // echo "{$variavel} e mais um pouco\n";
+    // $array = [];
+    // var_dump($array);
+
+?>
