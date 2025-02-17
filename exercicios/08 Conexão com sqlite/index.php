91d0380ad37fbfe257eea68e26c7166e48a12bac
Wallyson Martins de Lima <wallysonml13@gmail.com>
2025-02-17 19:52:54 -0300

    17022025

--- exercicios/08 Conexão com sqlite/index.php
+++ exercicios/08 Conexão com sqlite/index.php
@@ -7,7 +7,8 @@ $queries = [
     'cadastrar' => $db->prepare("insert into usuarios(nome, email, senha) values (:nome, :email, :senha)"),
     'mostrarTodos' => $db->prepare("select * from usuarios"),
     'deletar' => $db->prepare("delete from usuarios where id = :id"),
-    'mostrarPorEmail' => $db->prepare("select * from usuarios where email = :email")
+    'mostrarPorEmail' => $db->prepare("select * from usuarios where email = :email"),
+    'alterar' => $db->prepare("update usuarios set nome = :nome, email = :email, senha = :senha where email = :cadastro")
 ];
 
 
@@ -27,22 +28,25 @@ while ($menu != 6) {
     $menu = readline();
     switch ($menu) {
         case 1: // cadastrar
-            do {
-                echo "\nEntre com o nome:\n";
-                $n = readline();
-                echo "Entre com o email:\n";
-                $e = readline();
-                echo "Entre com a senha:\n";
-                $s = readline();
-            } while (!verificaEmail($e, $db));
-            echo "***Usuário cadastrado com sucesso!***\n";
+            echo "\nEntre com o nome:\n";
+            $n = readline();
+            echo "Entre com o email:\n";
+            $e = readline();
+            echo "Entre com a senha:\n";
+            $s = readline();
 
-            $queries['cadastrar']->execute([
-                'nome' => $n,
-                'email' => $e,
-                'senha' => $s
-            ]);
-            break;
+            if (verificaEmail($e, $db)) {
+                $queries['cadastrar']->execute([
+                    'nome' => $n,
+                    'email' => $e,
+                    'senha' => $s
+                ]);
+                echo "\n***Usuário cadastrado com sucesso!***";
+                break;
+            } else {
+                echo "\n***Email JÁ está em uso! Tente novamente!***";
+                break;
+            }
 
         case 2: // mostrar todos
             $queries['mostrarTodos']->execute();
@@ -59,33 +63,62 @@ while ($menu != 6) {
             break;
 
         case 3: // deletar
-            do {
-                echo "Entre com um email cadastrado:\n";
-                $e = readline();
-            } while (verificaEmail($e, $db));
-            $queries['mostrarPorEmail']->execute(['email' => $e]);
-            $u = $queries['mostrarPorEmail']->fetch();
-            echo "------------------------------------------\n";
-            echo "ID: " . $u['id'] . "\n";
-            echo "Nome: " . $u['nome'] . "\n";
-            echo "Email: " . $u['email'] . "\n";
-            echo "Senha: " . $u['senha'] . "\n";
-            echo "------------------------------------------\n";
-            echo "Deseja realmente deletar este cadastro?(s/n)\n";
-            $opt = readline();
-            if ($opt == 's') {
-                $queries['deletar']->execute(['id'=>$u['id']]);
-            }elseif ($opt == 'n') {
-                break;
+            echo "Entre com um email cadastrado:\n";
+            $e = readline();
+            if (!verificaEmail($e, $db)) {
+                $queries['mostrarPorEmail']->execute(['email' => $e]);
+                $u = $queries['mostrarPorEmail']->fetch();
+                echo "------------------------------------------\n";
+                echo "ID: " . $u['id'] . "\n";
+                echo "Nome: " . $u['nome'] . "\n";
+                echo "Email: " . $u['email'] . "\n";
+                echo "Senha: " . $u['senha'] . "\n";
+                echo "------------------------------------------\n";
+                echo "Deseja realmente deletar este cadastro?(s/n)\n";
+                $opt = readline();
+                if ($opt == 's') {
+                    $queries['deletar']->execute(['id' => $u['id']]);
+                    echo "\n***Usuário deletado!***";
+                } elseif ($opt == 'n') {
+                    break;
+                }
+            } else {
+                echo "***Email NÃO encontrado!***\n";
             }
             break;
 
         case 4: // buscar por email
-
+            echo "Entre com um email cadastrado:\n";
+            $e = readline();
+            if (!verificaEmail($e, $db)) {
+                $queries['mostrarPorEmail']->execute(['email' => $e]);
+                $u = $queries['mostrarPorEmail']->fetch();
+                echo "------------------------------------------\n";
+                echo "ID: " . $u['id'] . "\n";
+                echo "Nome: " . $u['nome'] . "\n";
+                echo "Email: " . $u['email'] . "\n";
+                echo "Senha: " . $u['senha'] . "\n";
+                echo "------------------------------------------\n";
+            } else {
+                echo "***Email NÃO encontrado!***\n";
+            }
             break;
 
         case 5: // alterar 
-
+            echo "Entre com um email cadastrado:\n";
+            $cadastro = readline();
+            if (!verificaEmail($cadastro, $db)) {
+                echo "\nEntre com o novo nome:\n";
+                $n = readline();
+                echo "Entre com o novo email:\n";
+                $e = readline();
+                echo "Entre com a nova senha:\n";
+                $s = readline();
+                $queries['alterar']->execute(['nome' => $n, 'email' => $e, 'senha' => $s, 'cadastro' => $cadastro]);
+                echo "\n***Cadastro atualizado com sucesso!***";
+            } else {
+                echo "***Email NÃO encontrado!***\n";
+            }
             break;
 
         case 6: // sair
@@ -105,10 +138,10 @@ function verificaEmail($email, $db)
     $t = $query->execute(['email' => $email]);
     //echo $t; //teste
     if ($query->fetch() == null) {
-        //echo "\n***Email disponível***\n";
+        //echo "\n***Email disponível/não cadastrado***\n";
         return true;
     } else {
-        //echo "\n***Email JÁ cadastrado!***\n";
+        //echo "\n***Email indisponível/JÁ cadastrado!***\n";
         return false;
     }
 }
