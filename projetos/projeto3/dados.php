d7e224161140f9c5365c440ee8c5067726f6f68b
Wallyson Martins de Lima <wallysonml13@gmail.com>
2025-04-08 21:53:32 -0300

    08042025

---
+++ projetos/projeto3/dados.php
@@ -0,0 +1,18 @@
+<?php 
+
+$livros = [
+    ['id' => 1, 'titulo' => 'O Senhor dos Anéis: A Sociedade do Anel 1', 'autor' => 'J. R. R. Tolkien', 'descricao' => 'A Sociedade do Anel é a primeira parte da trilogia O Senhor dos Anéis, de J.R.R. Tolkien. Nela, Frodo Bolseiro e oito companheiros partem em uma jornada para destruir o Um Anel e salvar a Terra-média.'],
+    ['id' => 2, 'titulo' => 'O Senhor dos Anéis: A Sociedade do Anel 2', 'autor' => 'J. R. R. Tolkien', 'descricao' => 'A Sociedade do Anel é a primeira parte da trilogia O Senhor dos Anéis, de J.R.R. Tolkien. Nela, Frodo Bolseiro e oito companheiros partem em uma jornada para destruir o Um Anel e salvar a Terra-média.'],
+    ['id' => 3, 'titulo' => 'O Senhor dos Anéis: A Sociedade do Anel 3', 'autor' => 'J. R. R. Tolkien', 'descricao' => 'A Sociedade do Anel é a primeira parte da trilogia O Senhor dos Anéis, de J.R.R. Tolkien. Nela, Frodo Bolseiro e oito companheiros partem em uma jornada para destruir o Um Anel e salvar a Terra-média.'],
+    ['id' => 4, 'titulo' => 'O Senhor dos Anéis: A Sociedade do Anel 4', 'autor' => 'J. R. R. Tolkien', 'descricao' => 'A Sociedade do Anel é a primeira parte da trilogia O Senhor dos Anéis, de J.R.R. Tolkien. Nela, Frodo Bolseiro e oito companheiros partem em uma jornada para destruir o Um Anel e salvar a Terra-média.'],
+    ['id' => 5, 'titulo' => 'O Senhor dos Anéis: A Sociedade do Anel 5', 'autor' => 'J. R. R. Tolkien', 'descricao' => 'A Sociedade do Anel é a primeira parte da trilogia O Senhor dos Anéis, de J.R.R. Tolkien. Nela, Frodo Bolseiro e oito companheiros partem em uma jornada para destruir o Um Anel e salvar a Terra-média.'],
+    ['id' => 6, 'titulo' => 'O Senhor dos Anéis: A Sociedade do Anel 6', 'autor' => 'J. R. R. Tolkien', 'descricao' => 'A Sociedade do Anel é a primeira parte da trilogia O Senhor dos Anéis, de J.R.R. Tolkien. Nela, Frodo Bolseiro e oito companheiros partem em uma jornada para destruir o Um Anel e salvar a Terra-média.'],
+    ['id' => 7, 'titulo' => 'O Senhor dos Anéis: A Sociedade do Anel 7', 'autor' => 'J. R. R. Tolkien', 'descricao' => 'A Sociedade do Anel é a primeira parte da trilogia O Senhor dos Anéis, de J.R.R. Tolkien. Nela, Frodo Bolseiro e oito companheiros partem em uma jornada para destruir o Um Anel e salvar a Terra-média.'],
+    ['id' => 8, 'titulo' => 'O Senhor dos Anéis: A Sociedade do Anel 8', 'autor' => 'J. R. R. Tolkien', 'descricao' => 'A Sociedade do Anel é a primeira parte da trilogia O Senhor dos Anéis, de J.R.R. Tolkien. Nela, Frodo Bolseiro e oito companheiros partem em uma jornada para destruir o Um Anel e salvar a Terra-média.'],
+    ['id' => 9, 'titulo' => 'O Senhor dos Anéis: A Sociedade do Anel 9', 'autor' => 'J. R. R. Tolkien', 'descricao' => 'A Sociedade do Anel é a primeira parte da trilogia O Senhor dos Anéis, de J.R.R. Tolkien. Nela, Frodo Bolseiro e oito companheiros partem em uma jornada para destruir o Um Anel e salvar a Terra-média.'],
+
+] 
+
+
+
+?>
---
+++ projetos/projeto3/livro.php
@@ -0,0 +1,60 @@
+<?php
+
+require 'dados.php';
+
+
+
+?>
+
+
+
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+
+<body class="bg-stone-950 text-stone-200">
+
+    <header class="bg-stone-900">
+        <nav class="mx-auto max-w-screen-lg flex justify-between px-8 py-4">
+            <div class="font-bold text-xl tracking-wide">Biblio Senac </div>
+            <ul class="flex space-x-4 font-bold">
+                <li><a href="/" class="text-lime-500">Explorar</a></li>
+                <li><a href="meus-livros" class="hover:underline">Meus Livros</a></li>
+            </ul>
+
+            <ul>
+                <li class="hover:underline">Fazer Login</li>
+            </ul>
+        </nav>
+    </header>
+
+    <main class="mx-auto max-w-screen-lg space-y-6">
+        <section class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-5">
+<!--  -->
+        <?php $livro = $livros[$_REQUEST['id']-1];?>
+                <div class="p-2 roudend border-stone-800 border-2 bg-stone-900 m-1">
+                    <div class="flex">
+                        <div class="w-1/3">Imagem</div>
+                        <div>
+                            <a href="/livro.php?id=<?= $livro['id']; ?>" class="font-semibold hover:underline"><?= $livro['titulo']; ?></a>
+                            <div class="text-xs italic"><?= $livro['autor']; ?></div>
+                            <div>⭐⭐⭐⭐⭐</div>
+                        </div>
+                    </div>
+                    <div>
+                    <?= $livro['descricao']; ?>
+                    </div>
+                </div>
+<!--  -->
+        </section>
+
+    </main>
+
+</body>
+
+</html>
